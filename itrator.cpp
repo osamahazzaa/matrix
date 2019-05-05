75faf44fcf6a69d52557e8d9b2be1b8955ba6f6a
osamahazzaa <50168368+osamahazzaa@users.noreply.github.com>
2019-05-06 01:02:45 +0200

    Update itrator.cpp

--- itrator.cpp
+++ itrator.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include<string>
 using namespace std;
  float ruslt[100][100];
  void insert_m (float matrix[][100],int &x,int &y , string input){
@@ -9,7 +9,7 @@ if (input[0]=='['||input[0]==']'||input[0]==' '){  input.erase(0,1); continue;}
 if ( input[0]==';'){ x++;  y=0;
 input.erase(0,1); continue; }
 string::size_type sz;
-matrix[x][y] = stod(input,&sz);
+try {matrix[x][y] = stod(input,&sz);}catch(int e){cout << "error";}
 input.erase(0,sz);
 cout  << endl;
 y++;
@@ -18,17 +18,17 @@ x=x+1;
  }
 
  void display( float  m[][100] ,int x ,int y){
+     cout << "[  " ;
 	for (int n = 0; n < x; n++){
-            cout << "|  " ;
+
 		for (int j =0 ; j < y ; j++ ){
 
-string intstr = to_string( m[n][j]);
-cout << m[n][j] ;
-cout << string((10 -intstr.length()), ' ');
+cout << m[n][j] << " " ;
+cout << ";";
+}
 
-		}
-		cout << "|" << endl ;
-	}}
+}
+	cout << "] " << endl;	}
 
 void add ( float mat1[][100] , float mat2[][100] , int x , int y ){
 
@@ -68,17 +68,19 @@ int x,y ;
 char operat ;
 string input;
 float matrix[100][100];
+try{
 getline(cin,input);
 insert_m(matrix,x,y,input);
-display(matrix,x,y);
+//display(matrix,x,y);
 cin >> operat ;
 cin.ignore();
 input.clear();
-
+}catch(int e){ cout << "error";}
 int x1,y1;
 float matrix2[100][100];
-getline(cin,input);
-insert_m(matrix2,x1,y1,input);
+try{getline(cin,input);
+insert_m(matrix2,x1,y1,input);}catch(int e){cout << "error";}
+
 
 if(x1==x&&y1==y){
 
@@ -91,6 +93,6 @@ case '/' : { sub(matrix,matrix2,x,y); display(ruslt,x,y); break;}
 case '*' : {multi(matrix,matrix2,x,y); display(ruslt,x,y); break;}
 }
 
-}else{ cout << "erorr";}
+}else{ cout << "error";}
 return 0;
 }
